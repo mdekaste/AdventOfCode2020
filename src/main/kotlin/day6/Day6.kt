4e8adcb816238d2e97bdd3aaf3ccc2585e3b64ef
Michael de Kaste <Michael.de.Kaste@topicus.nl>
2020-12-07 13:51:48 +0100

    update

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -15,9 +15,4 @@ object Day6 : AdventOfCode(){
 
     override fun part1() = parsed.sumBy { it.reduce { acc, bitSet -> acc.apply { or(bitSet) } }.cardinality() }
     override fun part2() = parsed.sumBy { it.reduce { acc, bitSet -> acc.apply { and(bitSet) } }.cardinality() }
-
-//    private val parsed = input.split("\r\n\r\n").map { it.lines().map(String::toSet) }
-//
-//    override fun part1() = parsed.sumBy{ it.reduce { acc, set -> acc.union(set) }.size }
-//    override fun part2() = parsed.sumBy{ it.reduce { acc, set -> acc.intersect(set) }.size }
 }
--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -3,35 +3,27 @@ package day7
 import AdventOfCode
 
 object Day7 : AdventOfCode() {
-    val parsed: Map<String, List<Pair<Int, String>>> = parseInput(input)
+    private val parsed: Map<String, Map<String, Int>> = input.lines().map { line ->
+        val (main, rest) = line.split(" bags contain ")
+        main to when (rest) {
+            "no other bags." -> emptyMap()
+            else -> rest.split(", ").map {
+                it.substringAfter(" ").substringBefore(" bag") to it.substringBefore(" ").toInt()
+            }.toMap()
+        }
+    }.toMap()
 
-    override fun part1() = mutableMapOf("shiny gold" to true).let { mem -> parsed.keys.count { containsShinyGold(mem, it) } - 1 }
+    private const val GOLD = "shiny gold"
+
+    override fun part1() = mutableMapOf(GOLD to true).let { mem -> parsed.keys.count { containsShinyGold(mem, it) } - 1 }
 
     private fun containsShinyGold(memory: MutableMap<String, Boolean>, currentColor: String) : Boolean = memory.getOrPut(currentColor){
-        parsed.getValue(currentColor).any { containsShinyGold(memory, it.second) }
+        parsed.getValue(currentColor).any { containsShinyGold(memory, it.key) }
     }
 
-    override fun part2() = countBags(mutableMapOf(), "shiny gold") - 1
+    override fun part2() = countBags(mutableMapOf(), GOLD) - 1
 
     private fun countBags(memory: MutableMap<String, Int>, color: String) : Int = memory.getOrPut(color){
-        1 + parsed.getValue(color).sumBy { (amount, other) -> amount * countBags(memory, other) }
-    }
-
-    private fun parseInput(input: String) : Map<String, List<Pair<Int, String>>>{
-        return input.lines().map { line ->
-            val (main, rest) = line.split(" bags contain ")
-            main to when (rest) {
-                "no other bags." -> emptyList()
-                else -> rest.split(", ").map {
-                    it.substringBefore(" ").toInt() to it.substringAfter(" ").substringBefore(" bag")
-                }
-            }
-        }.toMap()
+        1 + parsed.getValue(color).entries.sumBy{ (name, amount) -> amount * countBags(memory, name) }
     }
 }
-
-
-//dark indigo bags contain 2 clear indigo bags.
-//posh chartreuse bags contain 2 faded blue bags, 2 pale plum bags, 2 posh coral bags, 4 vibrant bronze bags.
-//dim fuchsia bags contain 5 dark red bags, 1 muted green bag, 4 clear indigo bags.
-//shiny crimson bags contain no other bags.
